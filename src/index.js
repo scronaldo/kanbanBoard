c789f65e8a6c97bd03951a12395baa78d48238e4
Amazing <naturalnoneedy@gmail.com>
2019-08-07 17:51:27 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import {Provider} from 'react-redux';
-import store from './store';
+import Store from './store';
 import * as serviceWorker from './serviceWorker';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const { persistor, store } = Store();
+// console.log(perstistor);
+ReactDOM.render(
+<Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
+  <App />
+  </PersistGate>
+ </Provider>, document.getElementById('root')
+ );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,42 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from "../reducers/index";
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 
 
-const store = createStore(rootReducer,  applyMiddleware(thunk));
+// TODO: DIVE DEEPER INTO R-P
+// https://github.com/rt2zz/redux-persist#basic-usage
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(persistedReducer, applyMiddleware(thunk));
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
 
 
-if(process.env.NODE_ENV !== 'production') {
-   window.store = store;
- }
-//  store.getState();
 
-export default store;
+
+
+// import {createStore, applyMiddleware} from 'redux';
+// import rootReducer from "../reducers/index";
+// import thunk from 'redux-thunk';
+
+
+
+// const store = createStore(rootReducer,  applyMiddleware(thunk));
+
+
+// if(process.env.NODE_ENV !== 'production') {
+//    window.store = store;
+//  }
+// //  store.getState();
+
+// export default store;
