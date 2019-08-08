1db972d491ed99c3932d4fc7bac1abfa5221b37d
Pavel <alwayssanmw@gmail.com>
2019-08-08 05:56:19 +0300

    Add files via upload

---
+++ build/precache-manifest.2227c649c0e202db00f216cea7b8cf74.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29c2383a6e2cc1575fa73312eb5331c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d88ba97a7579ba44523e",
+    "url": "/static/js/2.5e23656f.chunk.js"
+  },
+  {
+    "revision": "36c662db9821ad1b23f5",
+    "url": "/static/js/main.cf6ff0ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
